15a95b754b6cb1d485648d9e08d5e26b757aa8be
krnick <sungboss2004@gmail.com>
2020-08-27 17:03:24 +0800

    update graph

--- quark/utils/graph.py
+++ quark/utils/graph.py
@@ -12,17 +12,15 @@ def call_graph(first_wrappers_list, second_wrappers_list,
     mutual_method_name = None
     mutual_class_name = None
 
-    # Convert MUTF8String to string
-    first_wrappers_list = [str(x) for x in first_wrappers_list]
-    second_wrappers_list = [str(x) for x in second_wrappers_list]
-
     # Initialize the Digraph object
     dot = Digraph(filename=filename,
                   node_attr={"fontname": "Courier New Bold"},
                   comment="Quark-Engine Call Graph Result",
                   format="svg",
-                  # graph_attr={"label": crime_description},
+                  graph_attr={"label": "Potential Malicious Activity: " + crime_description, "labelloc": "top",
+                              "center": "true"},
                   )
+    dot.attr(compound='true')
 
     with dot.subgraph(name="cluster_mutual") as mutual_parent_function_description:
         mutual_parent_function_description.attr(style="rounded", penwidth="1", fillcolor="white",
@@ -31,31 +29,47 @@ def call_graph(first_wrappers_list, second_wrappers_list,
 
         # mutual parent function node
         mutual_class_name, mutual_method_name = (str(x) for x in mutual_parent_function)
-        mutual_class_name.replace(";", "")
         mutual_parent_function_description.node(mutual_method_name,
-                                                label=f"Class: {mutual_class_name}\nMethod: {mutual_method_name}",
+                                                label=f"Class: {mutual_class_name.replace(';', '')[1:]}\nMethod: {mutual_method_name}",
                                                 shape="none",
+                                                fontcolor="blue",
                                                 fontname="Courier New")
 
     with dot.subgraph(name="cluster_0") as wrapper:
         wrapper.attr(label="Wrapped Functions", fontname="Courier New Bold")
         wrapper.attr(style="rounded", penwidth="1", fillcolor="red", shape="box")
         # Build the first call nodes
-        for method_name in first_wrappers_list:
-            wrapper.node(method_name, label=method_name, style="rounded", penwidth="1", fillcolor="white",
+        for wp_func in first_wrappers_list:
+            wp_cls_name, wp_md_name = (str(x) for x in wp_func)
+
+            wrapper.node(wp_cls_name + wp_md_name,
+                         label=f"Class: {wp_cls_name.replace(';', '')[1:]}\nMethod: {wp_md_name}",
+                         style="rounded",
+                         fontcolor="blue",
+                         penwidth="1", fillcolor="white",
                          fontname="Courier New", shape="none")
 
         # wrapper -> wrapper
         for i in range(len(first_wrappers_list) - 1, 0, -1):
-            wrapper.edge(first_wrappers_list[i], first_wrappers_list[i - 1], "calls", fontname="Courier New")
+            wp_cls_name_1, wp_md_name_1 = (str(x) for x in first_wrappers_list[i])
+            wp_cls_name_2, wp_md_name_2 = (str(x) for x in first_wrappers_list[i - 1])
+            wrapper.edge(wp_cls_name_1 + wp_md_name_1, wp_cls_name_2 + wp_md_name_2, "calls", fontname="Courier New")
 
-        for method_name in second_wrappers_list:
-            wrapper.node(method_name, label=method_name, style="rounded", penwidth="1", fillcolor="white",
+        for wp_func in second_wrappers_list:
+            wp_cls_name, wp_md_name = (str(x) for x in wp_func)
+            wp_cls_name.replace(";", "")
+            wrapper.node(wp_cls_name + wp_md_name,
+                         label=f"Class: {wp_cls_name.replace(';', '')[1:]}\nMethod: {wp_md_name}",
+                         style="rounded",
+                         fontcolor="blue",
+                         penwidth="1", fillcolor="white",
                          fontname="Courier New", shape="none")
 
         # wrapper -> wrapper
         for i in range(len(second_wrappers_list) - 1, 0, -1):
-            wrapper.edge(second_wrappers_list[i], second_wrappers_list[i - 1], "calls", fontname="Courier New")
+            wp_cls_name_1, wp_md_name_1 = (str(x) for x in second_wrappers_list[i])
+            wp_cls_name_2, wp_md_name_2 = (str(x) for x in second_wrappers_list[i - 1])
+            wrapper.edge(wp_cls_name_1 + wp_md_name_1, wp_cls_name_2 + wp_md_name_2, "calls", fontname="Courier New")
 
     with dot.subgraph(name="cluster_1") as native_call_subgraph:
         native_call_subgraph.attr(style="rounded", penwidth="1", fillcolor="white",
@@ -66,14 +80,19 @@ def call_graph(first_wrappers_list, second_wrappers_list,
         second_native_api_class, second_native_method = second_native_call
         native_call_subgraph.node(first_native_method,
                                   label=f"Class: {first_native_api_class}\nMethod: {first_native_method}",
+                                  fontcolor="blue",
                                   shape="none", fontname="Courier New")
         native_call_subgraph.node(second_native_method,
                                   label=f"Class: {second_native_api_class}\nMethod: {second_native_method}",
+                                  fontcolor="blue",
                                   shape="none", fontname="Courier New")
 
     # mutual parent function -> the first node of each node
-    dot.edge(mutual_method_name, first_wrappers_list[-1], "First Call", fontname="Courier New")
-    dot.edge(mutual_method_name, second_wrappers_list[-1], "Second Call", fontname="Courier New")
+    first_wp_cls_name_1, first_wp_md_name_1 = (str(x) for x in first_wrappers_list[-1])
+    first_wp_cls_name_2, first_wp_md_name_2 = (str(x) for x in second_wrappers_list[-1])
+
+    dot.edge(mutual_method_name, first_wp_cls_name_1 + first_wp_md_name_1, "First Call", fontname="Courier New")
+    dot.edge(mutual_method_name, first_wp_cls_name_2 + first_wp_md_name_2, "Second Call", fontname="Courier New")
 
     dot.node("bottom", label="GPLv3 Quark-Engine\nhttps://github.com/quark-engine/quark-engine", shape="plaintext",
              fontname="Courier New")
@@ -89,16 +108,18 @@ def call_graph(first_wrappers_list, second_wrappers_list,
                                  fontname="Courier New", shape="none")
 
     # the last node of each node -> permission
-    dot.edge(first_wrappers_list[0], first_native_method, "calls", fontname="Courier New")
-    dot.edge(second_wrappers_list[0], second_native_method, "calls", fontname="Courier New")
+    last_wp_cls_name_1, last_wp_md_name_1 = (str(x) for x in first_wrappers_list[0])
+    last_wp_cls_name_2, last_wp_md_name_2 = (str(x) for x in second_wrappers_list[0])
+    dot.edge(last_wp_cls_name_1 + last_wp_md_name_1, first_native_method, "calls", fontname="Courier New")
+    dot.edge(last_wp_cls_name_2 + last_wp_md_name_2, second_native_method, "calls", fontname="Courier New")
 
-    dot.edge(first_native_method, "permission", "requests", fontname="Courier New")
-    dot.edge(second_native_method, "permission", "requests", fontname="Courier New")
+    dot.edge(first_native_method, "permission", "requests", fontname="Courier New", lhead='cluster_permission')
+    dot.edge(second_native_method, "permission", "requests", fontname="Courier New", lhead='cluster_permission')
 
     dot.edge("permission", "bottom", style="invis")
 
     # show the image
-    dot.view()
+    dot.render()
 
 
 if __name__ == '__main__':
