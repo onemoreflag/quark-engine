b57415a279db5a77d581849354c9403df2da1957
krnick <sungboss2004@gmail.com>
2020-08-27 16:18:38 +0800

    type1

--- quark/Objects/quark.py
+++ quark/Objects/quark.py
@@ -436,72 +436,68 @@ class Quark:
         # add the score
         self.score_sum += score
 
-
-        print(self.final_registerobject)
-        self.pre_method0.insert(0, rule_obj.x2n3n4_comb[0]["class"] + rule_obj.x2n3n4_comb[0]["method"])
-
-        self.pre_method1.insert(0, rule_obj.x2n3n4_comb[1]["class"] + rule_obj.x2n3n4_comb[1]["method"])
-        call_graph(self.pre_method0, self.pre_method1, mutual_parent_function=repr(self.mutual_parent_function),
+        call_graph(self.pre_method0, self.pre_method1, mutual_parent_function=self.mutual_parent_function,
                    crime_description=rule_obj.crime,
+                   first_natvie_call=(rule_obj.x2n3n4_comb[0]["class"], rule_obj.x2n3n4_comb[0]["method"]),
+                   second_native_call=(rule_obj.x2n3n4_comb[1]["class"], rule_obj.x2n3n4_comb[1]["method"]),
                    permission=rule_obj.x1_permission,
                    register_object=self.final_registerobject)
 
-    def show_detail_report(self, rule_obj):
-        """
-        Show the detail report.
+        def show_detail_report(self, rule_obj):
+            """
+            Show the detail report.
 
-        :param rule_obj: the instance of the RuleObject.
-        :return: None
-        """
+            :param rule_obj: the instance of the RuleObject.
+            :return: None
+            """
 
-        # Count the confidence
-        print("")
-        print(f"Confidence: {rule_obj.check_item.count(True) * 20}%")
-        print("")
-
-        if rule_obj.check_item[0]:
-
-            print(red(CHECK_LIST), end="")
-            print(green(bold("1.Permission Request")), end="")
+            # Count the confidence
+            print("")
+            print(f"Confidence: {rule_obj.check_item.count(True) * 20}%")
             print("")
 
-            for permission in rule_obj.x1_permission:
-                print(f"\t\t {permission}")
-        if rule_obj.check_item[1]:
-            print(red(CHECK_LIST), end="")
-            print(green(bold("2.Native API Usage")), end="")
-            print("")
+            if rule_obj.check_item[0]:
 
-            for class_name, method_name in self.level_2_reuslt:
-                print(f"\t\t ({class_name}, {method_name})")
-        if rule_obj.check_item[2]:
-            print(red(CHECK_LIST), end="")
-            print(green(bold("3.Native API Combination")), end="")
+                print(red(CHECK_LIST), end="")
+                print(green(bold("1.Permission Request")), end="")
+                print("")
 
-            print("")
-            print(
-                f"\t\t ({rule_obj.x2n3n4_comb[0]['class']}, {rule_obj.x2n3n4_comb[0]['method']})",
-            )
-            print(
-                f"\t\t ({rule_obj.x2n3n4_comb[1]['class']}, {rule_obj.x2n3n4_comb[1]['method']})",
-            )
-        if rule_obj.check_item[3]:
+                for permission in rule_obj.x1_permission:
+                    print(f"\t\t {permission}")
+            if rule_obj.check_item[1]:
+                print(red(CHECK_LIST), end="")
+                print(green(bold("2.Native API Usage")), end="")
+                print("")
 
-            print(red(CHECK_LIST), end="")
-            print(green(bold("4.Native API Sequence")), end="")
+                for class_name, method_name in self.level_2_reuslt:
+                    print(f"\t\t ({class_name}, {method_name})")
+            if rule_obj.check_item[2]:
+                print(red(CHECK_LIST), end="")
+                print(green(bold("3.Native API Combination")), end="")
 
-            print("")
-            print(f"\t\t Sequence show up in:")
-            for seq_method in self.same_sequence_show_up:
-                print(f"\t\t {repr(seq_method)}")
-        if rule_obj.check_item[4]:
+                print("")
+                print(
+                    f"\t\t ({rule_obj.x2n3n4_comb[0]['class']}, {rule_obj.x2n3n4_comb[0]['method']})",
+                )
+                print(
+                    f"\t\t ({rule_obj.x2n3n4_comb[1]['class']}, {rule_obj.x2n3n4_comb[1]['method']})",
+                )
+            if rule_obj.check_item[3]:
 
-            print(red(CHECK_LIST), end="")
-            print(green(bold("5.Native API Use Same Parameter")), end="")
-            print("")
-            for seq_operation in self.same_operation:
-                print(f"\t\t {repr(seq_operation)}")
+                print(red(CHECK_LIST), end="")
+                print(green(bold("4.Native API Sequence")), end="")
 
+                print("")
+                print(f"\t\t Sequence show up in:")
+                for seq_method in self.same_sequence_show_up:
+                    print(f"\t\t {repr(seq_method)}")
+            if rule_obj.check_item[4]:
 
-if __name__ == "__main__":
-    pass
+                print(red(CHECK_LIST), end="")
+                print(green(bold("5.Native API Use Same Parameter")), end="")
+                print("")
+                for seq_operation in self.same_operation:
+                    print(f"\t\t {repr(seq_operation)}")
+
+    if __name__ == "__main__":
+        pass
--- quark/utils/graph.py
+++ quark/utils/graph.py
@@ -6,7 +6,12 @@ def call_graph(first_wrappers_list, second_wrappers_list,
                crime_description=None,
                permission=None,
                register_object=None,
-               filename="quark_call_graph"):
+               filename="quark_call_graph",
+               first_natvie_call=None,
+               second_native_call=None):
+    mutual_method_name = None
+    mutual_class_name = None
+
     # Convert MUTF8String to string
     first_wrappers_list = [str(x) for x in first_wrappers_list]
     second_wrappers_list = [str(x) for x in second_wrappers_list]
@@ -16,41 +21,81 @@ def call_graph(first_wrappers_list, second_wrappers_list,
                   node_attr={"fontname": "Courier New Bold"},
                   comment="Quark-Engine Call Graph Result",
                   format="svg",
-                  graph_attr={"label": crime_description},
+                  # graph_attr={"label": crime_description},
                   )
 
-    # Build the nodes
-    for method_name in first_wrappers_list:
-        dot.node(method_name, label=method_name, shape="record", style="rounded")
+    with dot.subgraph(name="cluster_mutual") as mutual_parent_function_description:
+        mutual_parent_function_description.attr(style="rounded", penwidth="1", fillcolor="white",
+                                                fontname="Courier New", shape="box")
+        mutual_parent_function_description.attr(label="Mutual Parent Function", fontname="Courier New Bold")
 
-    for method_name in second_wrappers_list:
-        dot.node(method_name, label=method_name, shape="record", style="rounded")
+        # mutual parent function node
+        mutual_class_name, mutual_method_name = (str(x) for x in mutual_parent_function)
+        mutual_class_name.replace(";", "")
+        mutual_parent_function_description.node(mutual_method_name,
+                                                label=f"Class: {mutual_class_name}\nMethod: {mutual_method_name}",
+                                                shape="none",
+                                                fontname="Courier New")
 
-    # mutual parent function node
-    dot.node(mutual_parent_function, label=mutual_parent_function, shape="box")
-    # permission node
-    dot.node("permission", label="\n".join(permission))
-    # register node
-    dot.node("register", label=str(register_object).replace(",", "\n"))
+    with dot.subgraph(name="cluster_0") as wrapper:
+        wrapper.attr(label="Wrapped Functions", fontname="Courier New Bold")
+        wrapper.attr(style="rounded", penwidth="1", fillcolor="red", shape="box")
+        # Build the first call nodes
+        for method_name in first_wrappers_list:
+            wrapper.node(method_name, label=method_name, style="rounded", penwidth="1", fillcolor="white",
+                         fontname="Courier New", shape="none")
 
-    # Build the edges
+        # wrapper -> wrapper
+        for i in range(len(first_wrappers_list) - 1, 0, -1):
+            wrapper.edge(first_wrappers_list[i], first_wrappers_list[i - 1], "calls", fontname="Courier New")
 
-    # register object -> mutual parent function
-    dot.edge("register", mutual_parent_function)
+        for method_name in second_wrappers_list:
+            wrapper.node(method_name, label=method_name, style="rounded", penwidth="1", fillcolor="white",
+                         fontname="Courier New", shape="none")
+
+        # wrapper -> wrapper
+        for i in range(len(second_wrappers_list) - 1, 0, -1):
+            wrapper.edge(second_wrappers_list[i], second_wrappers_list[i - 1], "calls", fontname="Courier New")
+
+    with dot.subgraph(name="cluster_1") as native_call_subgraph:
+        native_call_subgraph.attr(style="rounded", penwidth="1", fillcolor="white",
+                                  fontname="Courier New", shape="box")
+        native_call_subgraph.attr(label="Native API Calls", fontname="Courier New Bold")
+        # Native API Calls
+        first_native_api_class, first_native_method = first_natvie_call
+        second_native_api_class, second_native_method = second_native_call
+        native_call_subgraph.node(first_native_method,
+                                  label=f"Class: {first_native_api_class}\nMethod: {first_native_method}",
+                                  shape="none", fontname="Courier New")
+        native_call_subgraph.node(second_native_method,
+                                  label=f"Class: {second_native_api_class}\nMethod: {second_native_method}",
+                                  shape="none", fontname="Courier New")
 
     # mutual parent function -> the first node of each node
-    dot.edge(mutual_parent_function, first_wrappers_list[-1], "calls")
-    dot.edge(mutual_parent_function, second_wrappers_list[-1], "calls")
+    dot.edge(mutual_method_name, first_wrappers_list[-1], "First Call", fontname="Courier New")
+    dot.edge(mutual_method_name, second_wrappers_list[-1], "Second Call", fontname="Courier New")
 
-    # wrapper -> wrapper
-    for i in range(len(first_wrappers_list) - 1, 0, -1):
-        dot.edge(first_wrappers_list[i], first_wrappers_list[i - 1], "calls")
-    # wrapper -> wrapper
-    for i in range(len(second_wrappers_list) - 1, 0, -1):
-        dot.edge(second_wrappers_list[i], second_wrappers_list[i - 1], "calls")
+    dot.node("bottom", label="GPLv3 Quark-Engine\nhttps://github.com/quark-engine/quark-engine", shape="plaintext",
+             fontname="Courier New")
+
+    with dot.subgraph(name="cluster_permission") as permission_subgraph:
+        permission_subgraph.attr(style="rounded", penwidth="1", fillcolor="white",
+                                 shape="box")
+        permission_subgraph.attr(label="Required Permissions", fontname="Courier New Bold")
+
+        # permission node
+        permission_subgraph.node("permission", label="\n".join(permission),
+                                 fillcolor="white",
+                                 fontname="Courier New", shape="none")
+
     # the last node of each node -> permission
-    dot.edge(first_wrappers_list[0], "permission", "request")
-    dot.edge(second_wrappers_list[0], "permission", "request")
+    dot.edge(first_wrappers_list[0], first_native_method, "calls", fontname="Courier New")
+    dot.edge(second_wrappers_list[0], second_native_method, "calls", fontname="Courier New")
+
+    dot.edge(first_native_method, "permission", "requests", fontname="Courier New")
+    dot.edge(second_native_method, "permission", "requests", fontname="Courier New")
+
+    dot.edge("permission", "bottom", style="invis")
 
     # show the image
     dot.view()
